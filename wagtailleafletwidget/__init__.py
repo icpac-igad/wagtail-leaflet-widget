fb8267b194d2bd35d47f92ed83029fc498957e6b
Erick Otenyo <otenyo.erick@gmail.com>
2019-07-29 13:59:39 +0300

    Bump version

--- wagtailleafletwidget/__init__.py
+++ wagtailleafletwidget/__init__.py
@@ -9,7 +9,7 @@ Leaflet JS based wagtail geo-location widget.
 """
 
 __title__ = "wagtailleafletwidget"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Erick Otenyo"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019-Present ICPAC"
