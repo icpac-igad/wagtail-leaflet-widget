79577b5c2de209f458ad48babc3996f29529e892
Erick Otenyo <otenyo.erick@gmail.com>
2021-04-07 15:06:26 +0300

    Add github pypi publish action

--- setup.py
+++ setup.py
@@ -1,34 +1,17 @@
-import io
 import os
-import re
-import sys
+import subprocess
 
 from setuptools import find_packages, setup
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
+version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+assert "." in version
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+assert os.path.isfile("cf_remote/version.py")
+with open("wagtailleafletwidget/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{version}\n")
 
-
-# Convert markdown to rst
-try:
-    from pypandoc import convert
-    long_description = convert("README.md", "rst")
-except:  # NOQA
-    long_description = ""
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-version = ''
-
-with io.open('wagtailleafletwidget/__init__.py', 'r', encoding='utf8') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
 
 setup(
     name='wagtailleafletwidget',
@@ -38,9 +21,10 @@ setup(
     license='MIT License',
     description='Leaflet JS based wagtail geo-location widget.',
     long_description=long_description,
-    url='https://github.com/wagtail-leaflet-widget',
+    long_description_content_type="text/markdown",
+    url='https://github.com/icpac-igad/wagtail-leaflet-widget',
     author='Erick Otenyo',
-    author_email='otenyo.erick@gmail.com',
+    author_email='eotenyo@icpac.net',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Wagtail',
--- wagtailleafletwidget/__init__.py
+++ wagtailleafletwidget/__init__.py
@@ -1,5 +1,4 @@
-
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
@@ -9,7 +8,6 @@ Leaflet JS based wagtail geo-location widget.
 """
 
 __title__ = "wagtailleafletwidget"
-__version__ = "1.0.1"
 __author__ = "Erick Otenyo"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019-Present ICPAC"
